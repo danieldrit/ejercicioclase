241208958ef818328d9105c15fb4a3f4c6a053bf
Daniel Obregozo <sala@unihorizonte.salas>
2019-11-01 15:39:25 -0500

    Proyecto inicial

---
+++ SenaFinal/src/senafinal/SenaFinal.java
@@ -0,0 +1,22 @@
+
+package senafinal;
+
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+
+public class SenaFinal {
+
+    public static void main(String[] args) {
+     int año=2019,edads,i;
+     Scanner edad = new Scanner(System.in);
+     System.out.println("Ingrese su Año de nacimiento");
+      i= edad.nextInt();
+     System.out.println("Su año es:"+i);
+      
+     edads = i-año;
+      System.out.println("Su edad es:"+edads);
+     
+    }
+    
+}
