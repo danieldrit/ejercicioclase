069dbc90eec5eace9af26a00b83a094e2e0d1cd0
Daniel <danieldrit@gmail.com>
2019-11-01 17:40:59 -0500

    actualizacion Francy

--- SenaFinal/src/senafinal/SenaFinal.java
+++ SenaFinal/src/senafinal/SenaFinal.java
@@ -9,7 +9,10 @@ public class SenaFinal {
 
     public static void main(String[] args) {
      int año=2019,edads,i;
+     String nombre;
      Scanner edad = new Scanner(System.in);
+      System.out.println("Ingrese su nombre");
+      nombre = edad.nextLine();
      System.out.println("Ingrese su Año de nacimiento");
       i= edad.nextInt();
      System.out.println("Su año es:"+i);
